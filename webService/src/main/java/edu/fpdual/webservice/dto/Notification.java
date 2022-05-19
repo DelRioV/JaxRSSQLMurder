ece2b065637ffa1ed33e3a9517c66b51c6a10954
DelRioV <pablosalvadordelriovergara.sanjose@alumnado.fundacionloyola.net>
2022-05-19 15:16:32 +0200

    Adding ping to webservice

---
+++ webService/src/main/java/edu/fpdual/webservice/dto/Notification.java
@@ -0,0 +1,12 @@
+package edu.fpdual.webservice.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Notification {
+    private int id;
+    private String title;
+    private String body;
+}
