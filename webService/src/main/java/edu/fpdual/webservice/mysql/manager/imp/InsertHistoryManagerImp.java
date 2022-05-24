d455ba22850626069794bbc029306e59d2a59af7
ismaelor03 <xxismaelor03xx@gmail.com>
2022-05-24 11:34:05 +0200

    Insert history before register done

--- webService/src/main/java/edu/fpdual/webservice/mysql/manager/imp/InsertHistoryManagerImp.java
+++ webService/src/main/java/edu/fpdual/webservice/mysql/manager/imp/InsertHistoryManagerImp.java
@@ -10,17 +10,15 @@ public class InsertHistoryManagerImp implements InsertHistoryManager {
 
 
 
-    private static final String HISTORYQUERY="Insert into History select distinct History_Id,?,Characters_id,Rooms_id" +
-            "from History_admin";
-    private static final String CHARACTERSQUERY="Insert into Characters select distinct ?,Character_id,Name,Features," +
-            "Personality,Version_Of_History from Characters_admin";
+    private static final String CHARACTERSQUERY="Insert into Characters select distinct ?,Character_id,Name,features," +
+            "Personality from Characters_admin";
 
     private static final String ROOMSQUERY="insert into Rooms" +
             "select distinct ?,RoomId,RoomName,RoomLocation,Description" +
             "from Rooms_admin";
 
-    private static final String MAINHISTORYSQUERY="Insert into Main_History select distict History_id,Description,? from" +
-            "Main_History_Admin";
+    private static final String HOURSSQUERY="Insert into Hours select distict character_id,?,Hours,Register from" +
+            "Hours_admin";
 
     @Override
     public boolean insertValues(Connection con, int user_code) throws SQLException {
@@ -28,8 +26,7 @@ public class InsertHistoryManagerImp implements InsertHistoryManager {
         try {
             roomsQueryInsert(con, user_code);
             charactersQueryInsert(con, user_code);
-            mainHistoryQueryInsert(con, user_code);
-            historyQueryInsert(con, user_code);
+            hoursQueryInsert(con, user_code);
             kk = true;
         } catch (Exception e){
             e.printStackTrace();
@@ -43,14 +40,10 @@ public class InsertHistoryManagerImp implements InsertHistoryManager {
         preparedStatement.executeUpdate();
     }
 
-    public void historyQueryInsert(Connection con, int user_code) throws SQLException {
-        PreparedStatement preparedStatement = con.prepareStatement(HISTORYQUERY);
-        preparedStatement.setInt(1,user_code);
-        preparedStatement.executeUpdate();
-    }
 
-    public void mainHistoryQueryInsert(Connection con, int user_code) throws SQLException {
-        PreparedStatement preparedStatement = con.prepareStatement(MAINHISTORYSQUERY);
+
+    public void hoursQueryInsert(Connection con, int user_code) throws SQLException {
+        PreparedStatement preparedStatement = con.prepareStatement(HOURSSQUERY);
         preparedStatement.setInt(1,user_code);
         preparedStatement.executeUpdate();
     }
